0f160e2ee1a38c9fd82f6489ae49962e3160015a
Yuhanna kapali <yuhannakapali@gmail.com>
2025-04-06 11:44:54 +0545

    feat: initail echo project init and project structure setuo

---
+++ backend/main.go
@@ -0,0 +1,7 @@
+package backend
+
+import "fmt"
+
+func Print(){
+	 fmt.Println("Hello World");
+}
---
+++ bit-torrent/bittorrent.go
@@ -0,0 +1,8 @@
+package bittorrent
+
+type bencodeInfo struct {
+    Pieces      string `bencode:"pieces"`
+    PieceLength int    `bencode:"piece length"`
+    Length      int    `bencode:"length"`
+    Name        string `bencode:"name"`
+}
